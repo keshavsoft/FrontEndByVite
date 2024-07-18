0e84d863a5969c2aae58e47872577f84d2d982f8
keshavsoft <nknnkumar@yahoo.com>
2024-07-18 12:41:58 +0530

    validations-deleteBreadcrumb

--- src/WithSSR/Simple/vite.config.js
+++ src/WithSSR/Simple/vite.config.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const SrcFolder = "src/WithSSR/Simple/SourceCode";
 
-const FrontEndDistFolder = "publicDir/binWithClientValid";
+const FrontEndDistFolder = "publicDir/binSimple";
 
 const root = resolve(__dirname, SrcFolder);
 
