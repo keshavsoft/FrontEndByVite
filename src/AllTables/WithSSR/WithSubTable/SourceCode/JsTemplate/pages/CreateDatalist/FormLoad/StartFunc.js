8783f7587e1e339b1ed8708ae45f867e5f766493
keshavsoft <nknnkumar@yahoo.com>
2024-07-19 23:05:20 +0530

    fixed all with sub table

--- src/AllTables/WithSSR/WithSubTable/SourceCode/JsTemplate/pages/CreateDatalist/FormLoad/StartFunc.js
+++ src/AllTables/WithSSR/WithSubTable/SourceCode/JsTemplate/pages/CreateDatalist/FormLoad/StartFunc.js
@@ -1,11 +1,13 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 import { StartFunc as ToLocalStorage } from "./ToLocalStorage/StartFunc.js";
 import { StartFunc as ToDataList } from "./ToDataList/StartFunc.js";
+import { StartFunc as ShowOnDom } from "./ShowOnDom/EntryFile.js";
 
 const StartFunc = async () => {
     StartFuncAddListeners();
     ToLocalStorage();
     ToDataList();
+    ShowOnDom();
 };
 
 export { StartFunc };
