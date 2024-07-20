2fd7b42598bb0ce24d1e4ca7bbc9c59085e52d93
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-07-20 18:01:28 +0530

    keshav

---
+++ src/AllTables/WithSSR/SubTableSideBar/KCode/generateVariables.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import { StartFunc as getColumnsData } from "./getColumnsData.js";
+import { StartFunc as GetTableNames } from "./GetTableNames.js";
+import { StartFunc as ReadDataSchema } from "./ReadDataSchema.js";
+import { StartFunc as foreignTableColumnsConfig } from "./foreignTableColumnsConfig.js";
+
+const StartFunc = ({ mode, inFilesArray, inSrcPath }) => {
+    const variables = {};
+    let LocalFiles = inFilesArray;
+    // let LoopInsidecolumnData = getColumnsData({ inSrcPath, inTableName });
+    let LocalTableNames = GetTableNames();
+    const sidebarItems = fs.readFileSync(`${inSrcPath}/KCode/sideBarItems.json`, { encoding: 'utf8' });
+    const LocalTablesWithSchema = ReadDataSchema();
+    // console.log("LocalTablesWithSchema : ", LocalTableNames);
+
+    Object.keys(LocalFiles).forEach((filename) => {
+        variables[filename + '.html'] = {
+            web_title: "Mazer Admin Dashboard",
+            filename: filename + '.html',
+            sidebarItems: JSON.parse(sidebarItems),
+            isDev: mode === 'development',
+            tableName: "",
+            columnData: {}
+        };
+
+        let LoopInsideTableName = LocalTableNames.find(element => {
+            return filename.startsWith(element);
+        });
+
+        if (LoopInsideTableName === undefined === false) {
+            let LoopInsidecolumnData = getColumnsData({ inSrcPath, inTableName: LoopInsideTableName });
+
+            variables[filename + '.html'].tableName = LoopInsideTableName;
+            variables[filename + '.html'].columnData = JSON.parse(LoopInsidecolumnData);
+
+            let LocalInsideForeignTable = foreignTableColumnsConfig({ inTableName: LoopInsideTableName, inTableWithColumnData: LocalTablesWithSchema });
+
+            variables[filename + '.html'].subTableName = LocalInsideForeignTable?.tableName;
+            variables[filename + '.html'].foreignTablecolumnData = LocalInsideForeignTable?.ColumnInfo;
+        };
+    });
+
+    return variables;
+};
+
+export { StartFunc };
---
+++ src/AllTables/WithSSR/SubTableSideBar/KCode/getColumnsData.js
@@ -0,0 +1,24 @@
+import path from "path";
+import fs from "fs";
+
+const StartFunc = ({ inTableName }) => {
+    let LocalColumnsPath = "ColumnSchema";
+
+    if (LocalFuncIsTablePresent({ inTableName }) === false) false;
+
+    const data = fs.readFileSync(`${LocalColumnsPath}/${inTableName}.json`, { encoding: 'utf8' });
+
+    return data;
+};
+
+const LocalFuncIsTablePresent = ({ inTableName }) => {
+    let LocalColumnsPath = "ColumnSchema";
+
+    let LocalFiles = fs.readdirSync(LocalColumnsPath).map(element => {
+        return path.parse(element).name;
+    });
+
+    return LocalFiles.includes(inTableName);
+};
+
+export { StartFunc };
