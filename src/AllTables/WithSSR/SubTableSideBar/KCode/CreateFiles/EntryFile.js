2d514689a2ad387e80575f54d3c2617a088d5ab7
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-07-20 18:06:14 +0530

    fixed error for new binATSubTableSideBar written code removed sidebar create

--- src/AllTables/WithSSR/SubTableSideBar/KCode/CreateFiles/EntryFile.js
+++ src/AllTables/WithSSR/SubTableSideBar/KCode/CreateFiles/EntryFile.js
@@ -1,10 +1,10 @@
 import { StartFunc as HtmlFiles } from "./HtmlFiles.js";
-import { StartFunc as sideBarItems } from "./sideBarItems.js";
+// import { StartFunc as sideBarItems } from "./sideBarItems.js";
 import { StartFunc as jsFiles } from "./jsFiles.js";
 
 const StartFunc = ({ inSrcPath, inSourceFolderName }) => {
     HtmlFiles({ inSrcPath, inSourceFolderName });
-    sideBarItems({ inSrcPath });
+    // sideBarItems({ inSrcPath });
     jsFiles({ inSrcPath, inSourceFolderName });
 };
 
