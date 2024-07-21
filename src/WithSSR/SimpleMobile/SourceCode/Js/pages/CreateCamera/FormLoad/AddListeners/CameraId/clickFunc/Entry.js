e91dd21b5b79f1cf5003dea016b3efe0e767737c
keshavsoft <nknnkumar@yahoo.com>
2024-07-21 23:29:43 +0530

    completed camera capture

--- src/WithSSR/SimpleMobile/SourceCode/Js/pages/CreateCamera/FormLoad/AddListeners/CameraId/clickFunc/Entry.js
+++ src/WithSSR/SimpleMobile/SourceCode/Js/pages/CreateCamera/FormLoad/AddListeners/CameraId/clickFunc/Entry.js
@@ -1,28 +1,22 @@
 let StartFunc = () => {
+    // Prefer camera resolution nearest to 1280x720.
+    const constraints = {
+        audio: false,
+        video: { width: 700, height: 500 },
+    };
 
-    const files = chooseFile.files[0];
-
-};
-
-// Prefer camera resolution nearest to 1280x720.
-const constraints = {
-    audio: true,
-    video: { width: 1280, height: 720 },
-};
-
-navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((mediaStream) => {
-        const video = document.querySelector("video");
-        video.srcObject = mediaStream;
-        video.onloadedmetadata = () => {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((mediaStream) => {
+            const video = document.querySelector("video");
+            video.srcObject = mediaStream;
             video.play();
-        };
-    })
-    .catch((err) => {
-        // always check for errors at the end.
-        console.error(`${err.name}: ${err.message}`);
-    });
+        })
+        .catch((err) => {
+            // always check for errors at the end.
+            console.error(`${err.name}: ${err.message}`);
+        });
 
+};
 
 export { StartFunc };
