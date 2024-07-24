22c15b459259a771c28610e4a22ff5cfd318e1ae
keshavsoft <nknnkumar@yahoo.com>
2024-07-24 17:20:29 +0530

    ShowNoSerial new html added

--- src/WithSSR/Simple/SourceCode/Js/pages/TableFooterAddSSR/FormLoad/BuildBsTable/InitializeFuncs/onPostBody/EntryFile.js
+++ src/WithSSR/Simple/SourceCode/Js/pages/TableFooterAddSSR/FormLoad/BuildBsTable/InitializeFuncs/onPostBody/EntryFile.js
@@ -1,7 +1,14 @@
 import { StartFunc as StartFuncTableFooterSaveButtonId } from "./TableFooterSaveButtonId/EntryFile.js";
+let jVarCommonTableTagId = jVarGlobalTableTagId;
 
 const StartFunc = () => {
     StartFuncTableFooterSaveButtonId();
+    jFLocalSetFocus();
+};
+
+const jFLocalSetFocus = () => {
+    let jVarLocalTable = document.getElementById(`${jVarCommonTableTagId}`);
+    jVarLocalTable.querySelector("tfoot").querySelector("input").focus();
 };
 
 export { StartFunc };
