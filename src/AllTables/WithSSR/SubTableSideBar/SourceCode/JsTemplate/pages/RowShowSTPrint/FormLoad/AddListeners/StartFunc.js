30bc87a4ae89c99073da0bfd6fef5691a1de5de0
keshavsoft <nknnkumar@yahoo.com>
2024-08-02 22:00:51 +0530

    for bill print covered in atsubtable

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/RowShowSTPrint/FormLoad/AddListeners/StartFunc.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/RowShowSTPrint/FormLoad/AddListeners/StartFunc.js
@@ -1,8 +1,6 @@
-import { StartFunc as SaveButtonId } from "./SaveButtonId/EntryFile.js";
 import { StartFunc as StartFuncBsSubTable } from "./BsSubTable/EntryFile.js";
 
 let StartFunc = () => {
-    SaveButtonId();
     StartFuncBsSubTable();
 };
 
