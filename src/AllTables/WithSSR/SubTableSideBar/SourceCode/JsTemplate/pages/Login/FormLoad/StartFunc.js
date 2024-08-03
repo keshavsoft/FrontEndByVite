310f1be5422073baf26796870276ecdbe5ee20a8
keshavsoft <nknnkumar@yahoo.com>
2024-08-03 17:13:45 +0530

    token started

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/Login/FormLoad/StartFunc.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/Login/FormLoad/StartFunc.js
@@ -1,7 +1,16 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 
-const StartFunc = async () => {
+const StartFunc = () => {
     StartFuncAddListeners();
+};
+
+const jFLocalCheckToken = ({ inTokenName }) => {
+    const cookieValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(`${inTokenName}=`))
+        ?.split("=")[1];
+
+    return cookieValue;
 };
 
 export { StartFunc };
