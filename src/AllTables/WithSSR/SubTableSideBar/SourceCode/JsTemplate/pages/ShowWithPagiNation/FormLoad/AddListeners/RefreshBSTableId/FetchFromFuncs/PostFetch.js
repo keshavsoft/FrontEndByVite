8b8ef17af72c374d727ba966d695baf5efd96d17
Bheem <bhavanibayya1807@gmail.com>
2024-08-03 14:32:16 +0530

    completed login in atsubtable

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/ShowWithPagiNation/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/ShowWithPagiNation/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
@@ -1,7 +1,8 @@
-// import ConfigJson from '../../../../Config.json' with {type: 'json'};
+ import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalTableName = jVarGlobalTableName;
+    let jVarLocalRoutePath = ConfigJson.routePath;
 
     let jVarLocalFetchUrl = `/bin/${jVarLocalTableName}/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
