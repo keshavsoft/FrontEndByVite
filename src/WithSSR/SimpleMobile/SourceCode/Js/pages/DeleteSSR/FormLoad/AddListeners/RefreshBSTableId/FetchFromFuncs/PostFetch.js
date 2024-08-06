ac7faea72f3de58558564a4aed1a9a0f0de6c963
BhAvAnI-BaYyA <bhavanibayya1807@gmail.com>
2024-08-06 19:37:03 +0530

    ColumnSchema folder added

--- src/WithSSR/SimpleMobile/SourceCode/Js/pages/DeleteSSR/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
+++ src/WithSSR/SimpleMobile/SourceCode/Js/pages/DeleteSSR/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
@@ -1,9 +1,10 @@
-// import ConfigJson from '../../../../Config.json' with {type: 'json'};
+ import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalTableName = jVarGlobalTableName;
-
-    let jVarLocalFetchUrl = `/bin/${jVarLocalTableName}/DataOnly`;
+    let jVarLocalRoutePath = ConfigJson.routePath;
+    
+    let jVarLocalFetchUrl = `/${jVarLocalRoutePath}/${jVarLocalTableName}/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
 
     return await response;
