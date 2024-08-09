b24e658d663b00dcec7d7c10e5bcfcc314282993
keshavsoft <nknnkumar@yahoo.com>
2024-08-09 19:25:17 +0530

    fetch masters on load

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/CreateSubTable/FormLoad/StartFunc.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/CreateSubTable/FormLoad/StartFunc.js
@@ -2,17 +2,23 @@ import { StartFunc as ShowOnDom } from "./ShowOnDom/EntryFile.js";
 import { StartFunc as BuildBsTableForSubTable } from "./BuildBsTableForSubTable/EntryFile.js";
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 
-const StartFunc = async () => {
+const StartFunc = () => {
     ShowOnDom().then();
 
     BuildBsTableForSubTable();
 
     StartFuncAddListeners();
+    jFLocalFetchMasters();
     jFLocalFillDataLists();
 };
 
 const jFLocalFillDataLists = () => {
     let jVarLocalHeaderToDataListId = document.getElementById('HeaderToDataListId');
+    jVarLocalHeaderToDataListId.click();
+};
+
+const jFLocalFetchMasters = () => {
+    let jVarLocalHeaderToDataListId = document.getElementById('AllMastersHeaderId');
     jVarLocalHeaderToDataListId.click();
 };
 
