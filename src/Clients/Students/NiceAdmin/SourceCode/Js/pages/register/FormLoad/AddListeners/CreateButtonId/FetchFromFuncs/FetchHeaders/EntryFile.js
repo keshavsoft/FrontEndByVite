bb329b025dcdef73705a71f8e6489572a8fa49c6
keshavsoft <nknnkumar@yahoo.com>
2024-09-03 15:37:35 +0530

    register fixed

--- src/Clients/Students/NiceAdmin/SourceCode/Js/pages/register/FormLoad/AddListeners/CreateButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
+++ src/Clients/Students/NiceAdmin/SourceCode/Js/pages/register/FormLoad/AddListeners/CreateButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
@@ -1,6 +1,6 @@
 import KeysJson from './Keys.json' with {type: 'json'};
 
-let StartFunc = () => {
+let StartFunc = async () => {
     let jVarLocalKeys = { ...KeysJson };
 
     jVarLocalKeys.body = {};
@@ -9,9 +9,18 @@ let StartFunc = () => {
     jVarLocalKeys.body.Mobile = jFLocalMobileId();
     jVarLocalKeys.body.Email = jFLocalEmailId();
     jVarLocalKeys.body.College = jFLocalCollegeSelectId();
+
+    const position = await getCurrentPosition();
+    console.log(position)
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
+
+    jVarLocalKeys.body.latitude = lat;
+    jVarLocalKeys.body.longitude = lon;
+    console.log("jVarLocalKeys : ", jVarLocalKeys.body);
     jVarLocalKeys.body = JSON.stringify(jVarLocalKeys.body);
 
-    return jVarLocalKeys;
+    return await jVarLocalKeys;
 };
 
 let jFLocalStudentNameId = () => {
@@ -48,6 +57,39 @@ let jFLocalCollegeSelectId = () => {
     if (jVarLocalHtmlId === null === false) {
         return jVarLocalHtmlId.value.trim();
     };
+};
+
+function geoFindMe() {
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        let jVarLocalLocation = {};
+        jVarLocalLocation.latitude = latitude;
+        jVarLocalLocation.longitude = longitude;
+        console.log("jVarLocalLocation : ", jVarLocalLocation);
+
+        return jVarLocalLocation;
+    };
+
+    function error() {
+        console.log("aaaaaaaaeeeeee : ");
+    }
+
+    if (!navigator.geolocation) {
+        console.log("Geolocation is not supported by your browser");
+    } else {
+        console.log("Locating…");
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+};
+
+function getCurrentPosition() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            position => resolve(position),
+            error => reject(error)
+        )
+    })
 };
 
 export { StartFunc }
