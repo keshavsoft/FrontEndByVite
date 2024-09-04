7c6e3ddef8b43af6eb4495cf2420612b3cd09235
keshavsoft <nknnkumar@yahoo.com>
2024-09-04 15:56:21 +0530

    students image upload working

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/CreateCamera/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/CreateCamera/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
@@ -10,23 +10,6 @@ let StartFunc = () => {
     return KeysJson;
 };
 
-function takepicture() {
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    const width = 320;    // We will scale the photo width to this
-    const height = 300;
-    const photo = document.getElementById("photo");
-
-    if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
-
-        const data = photo.toDataURL("image/png");
-        return data;
-    };
-};
-
 const jFLocalImageToUrl = () => {
     var c = document.createElement('canvas');
     var img = document.getElementById('photo');
